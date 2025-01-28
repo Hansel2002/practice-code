1a9f3716b5c53ab34689ad1c2315f3e7be40cb3b
Hansel <alhansanradiamoda20@gmail.com>
2025-01-28 14:02:57 +0800

    recenly added

--- practice.sh
+++ practice.sh
@@ -2,3 +2,7 @@
 echo "Welcome to the bash"
 read -p "Enter your name: " name
 echo "Good, $name"
+read -p "Enter the first number: " num1
+read -p "Enter the secome number: " num2
+div=$((num1/num2))
+echo "the result is: $div"
