2c031997d60a3dcadd51b6c6eff4e5df49344a35
Hansel <alhansanradiamoda20@gmail.com>
2025-01-28 12:39:00 +0800

    Practice-Code (OJT)

---
+++ practice-simpel.sh
@@ -0,0 +1,6 @@
+#!/bash/bin
+read -p "Enter the first number" num1
+read -p "Enter the second number" num2
+sub=$((num1 - num2))
+echo "The result is $sub"
+
--- practice-simple.sh
+++ practice-simple.sh
@@ -1,1 +1,6 @@
-# practice-code
+#!/bin/bash
+
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+sub=$((num1-num2))
+echo "the result is $sub"
